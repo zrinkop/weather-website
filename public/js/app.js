7be821e31824d7e6e2d06141eae959f0cd3e7bf2
Zrinko Puljic <zpuljic@iolap.com>
2020-03-05 22:20:08 +0100

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,3 @@
-//client-side Javascript run in browser
-
-console.log('test111')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) =>{
-//         response.json().then((data) => {
-//             console.log(data);
-//     })
-// })
-
-
-// fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-//     response.json().then((data) => {
-
-//         if(data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.forecast)
-//             console.log(data.location)
-//         }
-        
-//         })
-// })
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
@@ -47,8 +23,5 @@ weatherForm.addEventListener('submit', (e) => {
         }
         
         })
-})
-
-
-
+    })
 })
